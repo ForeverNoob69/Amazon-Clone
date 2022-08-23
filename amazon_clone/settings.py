ba4d05c06fb1813396aef99481ea40d8314c94ea
ForeverNoob69 <100515357+ForeverNoob69@users.noreply.github.com>
2022-08-23 14:18:45 +0600

    Update settings.py

--- amazon_clone/settings.py
+++ amazon_clone/settings.py
@@ -186,5 +186,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    # 'http://localhost:3000/checkout',
+    'https://amazon-clone69.herokuapp.com',
 ]
